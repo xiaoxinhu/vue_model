bd2e0fa53bece398da097667e7a36cfa7ed149df
xiaoxin <2399431475@qq.com>
2021-11-08 19:48:48 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
 import './plugins/element.js'
-
+//导入全局样式表
+import './assets/css/global.css'
+//导入字体图表
+import './assets/fonts/iconfont.css'
+//导入axios库
+import axios from 'axios'
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http=axios
+
 
 new Vue({
   router,
---
+++ src/router/router.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', redirect: '/login'},
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
+})
+
+router.beforeEach((to,from,next)=>{ 
+  if(to.path === '/login')
+    return next();
+  
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token');
+
+  if(!tokenStr)
+    return next('/login');
+
+  next();
+
+})
+
+
+export default router
