65aa94e74852aa9d41a33fdd2ffa0280e91c6436
xiaoxin <2399431475@qq.com>
2021-11-10 20:40:11 +0800

    完成用户列表功能的开发

--- src/router/router.js
+++ src/router/router.js
@@ -2,14 +2,26 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
+import Welcome from '../views/Welcome.vue'
+import Users from '../views/user/Users.vue'
+
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/', redirect: '/login'},
+    { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    { 
+      path: '/home', 
+      component: Home, 
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome},
+        { path: '/users', component: Users}  
+      ]
+  
+    }
   ]
 })
 
